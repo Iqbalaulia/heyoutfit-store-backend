99b1927aa36925439d7f0a8e3b82ef06361490ae
Iqbalaulia <miqbalar12@gmail.com>
2020-07-31 15:04:41 +0700

    create model product

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    // Menggunakan softdeletes
+    use SoftDeletes;
+
+    // Menghubungkan dengan database product
+    protected $fillable = [
+        'name', 'type', 'description', 'price', 'slug', 'quantity'
+    ];
+
+    // Menyembunyikan field yang tidak ingin ditampilkan
+    protected $hidden = [
+
+    ];
+
+    // Menghubungkan relasi product dengan table ProductGallery [0]
+    // [1] terdapat pada model ProductGallery
+    public function galleries(){
+        return $this->hasMany(ProductGallery::class,'products_id','id');
+    }
+
+}
