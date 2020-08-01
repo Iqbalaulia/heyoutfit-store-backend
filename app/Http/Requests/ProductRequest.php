cc4b271160c7866e00eacfb851d8e7995ad8f19b
Iqbalaulia <miqbalar12@gmail.com>
2020-08-01 13:32:19 +0700

    create form request validation for product

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // Dirubah menjadi true agar bisa terpakai
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //Aturan - aturan
+            'name' => 'requiered|max:255',
+            'type' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|integer',
+            'quantity' => 'required|integer'
+        ];
+    }
+}
